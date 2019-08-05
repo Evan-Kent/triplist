9fc5c002d35fd3024c91b40a00820f14f33e011b
Evan <ekent82@gmail.com>
2019-08-05 15:02:07 -0700

    initial commit, starter code developed on codesandbox.io

---
+++ src/column.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Task from "./task";
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+
+const ListContainer = styled.div`
+  margin: 20px;
+  border: 1px solid grey;
+  border-radius: 10px;
+`;
+
+const Title = styled.h2`
+  padding: 16px;
+`;
+
+const TaskList = styled.ul`
+  padding: 16px;
+`;
+
+export default class Column extends React.Component {
+    render() {
+        return (
+            <ListContainer>
+                <Title>{this.props.column.title}</Title>
+                <Droppable droppableId={this.props.column.id}>
+                    {(provided, snapshot) => (
+                        <TaskList
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {this.props.tasks.map((task, index) => (
+                                <Task key={task.id} task={task} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </ListContainer>
+        );
+    }
+}
---
+++ src/task.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
+
+const Container = styled.li`
+  border: 1px solid lightgray;
+  padding: 8px;
+  margin-bottom: 4px;
+  background-color: white;
+  border-radius: 5px;
+  list-style-type: none;
+`;
+
+export default class Task extends React.Component {
+    render() {
+        return (
+            <Draggable draggableId={this.props.task.id} index={this.props.index}>
+                {provided => (
+                    <Container
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                    >
+                        {this.props.task.content}
+                    </Container>
+                )}
+            </Draggable>
+        );
+    }
+}
