a17285db5a63f333aa2d47b840e7c190faf23192
Evan <ekent82@gmail.com>
2019-08-11 10:11:58 -0700

    revised firestore impmentation

--- src/firestore.js
+++
@@ -1,7 +0,0 @@
-// import firebase from "firebase";
-// // Required for side-effects
-// import "firebase/firestore";
-const API = require("../security.json");
-
-//const API = JSON.parse(security);
-console.log(API);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
-import { DragDropContext } from "react-beautiful-dnd";
-import initialData from "./initialData";
+import {DragDropContext} from "react-beautiful-dnd";
+//import initialData from "./initialData";
 import Column from "./column";
-
 import "./styles.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+
+const ApiConfig = require("./security.json");
+
 
 /************************************************************
  * A simple task list app by Evan Kent
@@ -15,6 +19,10 @@ import "./styles.css";
  * and https://medium.com/@colebemis
  */
 
+
+firebase.initializeApp(ApiConfig.firestore);
+const db = firebase.firestore();
+
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
@@ -23,14 +31,20 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      ...initialData,
+      loading: true,
       editing: false,
       tempContent: "",
+      input: "",
+      columns: {},
+      tasks: {},
+      columnOrder: [],
+      taskCount: 0,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -38,16 +52,52 @@ export default class App extends React.Component {
     this.finishEdit = this.finishEdit.bind(this);
   }
 
+  componentDidMount() {
+    this.getStarterList();
+  }
+
+  getStarterList = () => {
+    db.collection("lists")
+      .doc("initialData")
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log(this.state);
+          this.setInitialState(doc.data());
+        } else {
+          console.log("initialData does not exist!");
+        }
+      });
+  };
+
+  setInitialState = initialState => {
+    if (initialState.tasks &&
+      initialState.columns &&
+      initialState.columnOrder &&
+      initialState.taskCount) {
+      console.log("we good!");
+      this.setState({
+        tasks: initialState.tasks,
+        columns: initialState.columns,
+        columnOrder: initialState.columnOrder,
+        taskCount: initialState.taskCount,
+      }, () => {
+        this.setState({
+          loading: false,
+        })
+      });
+      console.log(this.state);
+    }
+  };
 
   onDragEnd = result => {
-    const { destination, source } = result;
+    const {destination, source} = result;
     // dropped outside the list
     if (!destination) {
       return;
     }
     if (destination.droppableId === source.droppableId
-      && destination.index === source.index)
-    {
+      && destination.index === source.index) {
       return
     }
 
@@ -63,12 +113,12 @@ export default class App extends React.Component {
       taskOrder: newTaskIds,
     };
 
-      this.setState({
-        columns: {
-          ...this.state.columns,
-          [newColumn.id]: newColumn,
-        }
-      });
+    this.setState({
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      }
+    });
   };
 
   handleSubmit = (event) => {
@@ -141,7 +191,7 @@ export default class App extends React.Component {
   handleDelete = (event, columnId, index) => {
     const column = this.state.columns[columnId];
     const taskOrder = column.taskOrder.slice(0, index).concat(
-      column.taskOrder.slice(index+1));
+      column.taskOrder.slice(index + 1));
 
     this.setState({
       columns: {
@@ -157,27 +207,30 @@ export default class App extends React.Component {
   };
 
   render() {
+    console.log("rendering!", this.state.loading);
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        {this.state.columnOrder.map(columnId => {
-          const column = this.state.columns[columnId];
-          const tasks = column.taskOrder.map(taskId => this.state.tasks[taskId]);
-          // console.log("order of taskIds", column.taskOrder);
-          // console.log("tasks passed to column: ", tasks);
-          return (
-            <Column
-              key={column.id}
-              column={column}
-              tasks={tasks}
-              submitHandler={this.handleSubmit}
-              changeHandler={this.handleChange}
-              editHandler={this.handleEdit}
-              deleteHandler={this.handleDelete}
-              input={this.state.input}
-            />
-          );
-        })}
-      </DragDropContext>
+      <>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskOrder.map(taskId => this.state.tasks[taskId]);
+            // console.log("order of taskIds", column.taskOrder);
+            // console.log("tasks passed to column: ", tasks);
+            return (
+              <Column
+                key={column.id}
+                column={column}
+                tasks={tasks}
+                submitHandler={this.handleSubmit}
+                changeHandler={this.handleChange}
+                editHandler={this.handleEdit}
+                deleteHandler={this.handleDelete}
+                input={this.state.input}
+              />
+            );
+          })}
+        </DragDropContext>
+      </>
     );
   }
 }
@@ -192,9 +245,4 @@ const TaskInput = ({...props}) => (
   </form>
 );
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+ReactDOM.render(<App/>, rootElement);
